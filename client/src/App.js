494741337269cf024b8b3e3024489fefa6e94555
ssukgoon <ssukgoon@gmail.com>
2024-11-05 15:16:38 +0900

    Chore: initialize React project setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import UploadPage from "./components/UploadPage"; // UploadPage 컴포넌트를 import
+
+function App() {
+  return (
+    <div className="App">
+      <UploadPage /> {/* UploadPage 컴포넌트를 렌더링 */}
+    </div>
+  );
+}
+
+export default App;
